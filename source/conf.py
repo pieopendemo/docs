426183a50319b168c351e30c2304f736a78dcfcf
Du Xinghua <xinghua.0108@hotmail.com>
2018-08-24 16:08:21 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# opendocs documentation build configuration file, created by
+# pieopendocs documentation build configuration file, created by
 # sphinx-quickstart on Tue Aug  7 14:15:38 2018.
 #
 # This file is execfile()d with the current directory set to its
@@ -45,7 +45,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'opendocs'
+project = u'pieopendocs'
 copyright = u'2018, pieopen'
 author = u'pieopen'
 
@@ -81,7 +81,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+#
 html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -99,7 +99,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'opendocsdoc'
+htmlhelp_basename = 'pieopendocsdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -107,7 +107,7 @@ htmlhelp_basename = 'opendocsdoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+    # 'papersize': 'letterpaper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
@@ -126,7 +126,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'opendocs.tex', u'opendocs Documentation',
+    (master_doc, 'pieopendocs.tex', u'pieopendocs Documentation',
      u'pieopen', 'manual'),
 ]
 
@@ -136,7 +136,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'opendocs', u'opendocs Documentation',
+    (master_doc, 'pieopendocs', u'pieopendocs Documentation',
      [author], 1)
 ]
 
@@ -147,8 +147,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'opendocs', u'opendocs Documentation',
-     author, 'opendocs', 'One line description of project.',
+    (master_doc, 'pieopendocs', u'pieopendocs Documentation',
+     author, 'pieopendocs', 'One line description of project.',
      'Miscellaneous'),
 ]
 
